f131d4c9e0580a4633ce4140e4547cd30056ddda
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-26 10:51:08 +0530

    using react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>PageOne</div>
+    );
+}
+
+const PageTwo = () => {
+    return (
+        <div>PageTwo</div>
+    );
+}
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 }
 

6b422b5083ace8f37f5ecf751c5ba19cfc9f3ee4
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-26 11:11:34 +0530

    navigating between pages using Link Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
     return (
-        <div>PageOne</div>
+        <div>
+            PageOne
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div>
     );
 }
 
 const PageTwo = () => {
     return (
-        <div>PageTwo</div>
+        <div>
+            PageTwo
+            <Link to="/">Navigate to Page One</Link>
+        </div>
     );
 }
 

83d330e2f6bf6a2167c84e4757b3df373e29f75b
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-26 11:29:04 +0530

    setup routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
-import StreaEdit from './streams/StreamEdit';
+import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
@@ -11,7 +11,11 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>

91a512d6033149d6eac0522b8d341136683e0329
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-27 11:00:48 +0530

    setting up GoogleAuth component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '758226845557-9dqdg33a1f36h39r58p66rnullbhqb80.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+
     render() {
         return (
             <div>Google Auth</div>

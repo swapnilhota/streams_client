297e044b17b9ab913002e6b5299e081f4863864b
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-27 11:07:35 +0530

    update GoogleAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
 
+    state = { isSignedIn: null };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '758226845557-9dqdg33a1f36h39r58p66rnullbhqb80.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }

a52408de848f5167446424f318b4291469528c77
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-30 14:44:58 +0530

    update to accomodate defined history

--- src/history.js
+++ src/history.js
@@ -1,3 +1,5 @@
 import createHistory from 'history/createBrowserHistory';  //automatically installed with react-router-dom
 
 export default createHistory();
+
+// to track navigation history of user

e32bca3d751e35785458dc40a8d4f03284a699fd
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-30 15:21:10 +0530

    loading stream in edit page

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>{this.props.stream.title}</div>
+        );
+    }
 }
 
-export default StreamEdit;
+// ownProps = props of this component
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps)(StreamEdit);

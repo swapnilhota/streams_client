28ddea989c0713498ea79fc6a2d79205f41deeed
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-30 14:21:44 +0530

    styling edit/delete buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,7 +12,14 @@ class StreamList extends React.Component {
     renderAdmin(stream) {
         if (stream.userId === this.props.currentUserId) {
             return (
-                <div>Edit/Delete</div>
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
             );
         }
     }
@@ -22,6 +29,7 @@ class StreamList extends React.Component {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -29,7 +37,6 @@ class StreamList extends React.Component {
                             {stream.description}
                         </div>
                     </div>
-                    {this.renderAdmin(stream)}
                 </div>
             );
         })

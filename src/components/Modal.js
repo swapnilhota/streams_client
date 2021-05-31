6771606f6ff4d1a6c8a141317c697e5074d35a35
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-31 11:47:18 +0530

    styling modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,16 @@ const Modal = (props) => {
     return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                Delete Stream
+                <div className="header">
+                    Delete Stream
+                </div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')

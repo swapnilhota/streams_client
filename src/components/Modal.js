7e889942a88a97c3598ea57926bd123e72ca42b0
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-31 11:49:12 +0530

    fixing modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
+        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
                 <div className="header">
                     Delete Stream
@@ -23,4 +24,4 @@ const Modal = (props) => {
 
 export default Modal;
 
-//to be used for delete streams
+//used for delete streams component

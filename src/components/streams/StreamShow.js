9ece376485aba5b52f980da211cc036f8e4fe3c2
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-01 11:10:44 +0530

    update StreamShow component

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    );
+class StreamShow extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        return (
+            <div> StreamShow</div >
+        );
+    }
+
 }
 
-export default StreamShow;
+const mapStateToProps = (state) => {
+
+}
+
+export default connect(null, { fetchStream })(StreamShow);

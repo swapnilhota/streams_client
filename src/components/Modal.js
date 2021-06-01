444fde485e4e68242dd67af2acfbe8813641c30b
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-01 10:10:22 +0530

    update modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,11 +10,10 @@ const Modal = (props) => {
                     {props.title}
                 </div>
                 <div className="content">
-                    Are you sure you want to delete this stream?
+                    {props.content}
                 </div>
                 <div className="actions">
-                    <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    {props.actions}
                 </div>
             </div>
         </div>,
